2a83a75e3ca3de6e16e052249922f219c7cca4a9
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-03 20:54:25 -0300

    feat: Add TokenRepository for managing authentication tokens

---
+++ src/repositories/TokenRepository.ts
@@ -0,0 +1,41 @@
+import CryptoJS from 'crypto-js'
+import Cookies from 'js-cookie'
+
+export class TokenRepository {
+  private static cookieKey = '@INNOVE:TOKEN'
+
+  public static get(): string | null {
+    const encryptedToken = Cookies.get(this.cookieKey)
+
+    if (!encryptedToken) {
+      return null
+    }
+
+    const wordsList = CryptoJS.AES.decrypt(
+      encryptedToken,
+      process.env.NEXT_PUBLIC_SECRET_ENCRYPT as string,
+    )
+
+    const decryptedToken = wordsList.toString(CryptoJS.enc.Utf8)
+    return decryptedToken
+  }
+
+  public static set(token: string): void {
+    const wordsList = CryptoJS.AES.encrypt(
+      token,
+      process.env.NEXT_PUBLIC_SECRET_ENCRYPT as string,
+    )
+
+    const encryptedToken = wordsList.toString()
+
+    Cookies.set(this.cookieKey, encryptedToken, {
+      secure: true,
+      sameSite: 'Strict',
+      expires: 3,
+    })
+  }
+
+  public static delete(): void {
+    Cookies.remove(this.cookieKey)
+  }
+}
