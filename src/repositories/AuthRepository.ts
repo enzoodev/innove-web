a263229b38cbcaab6d95133ea7e67c5ac84043ef
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-03 20:54:21 -0300

    feat: Add AuthRepository for user authentication

---
+++ src/repositories/AuthRepository.ts
@@ -0,0 +1,47 @@
+import { HttpServices } from '@/services/HttpServices'
+import { TokenRepository } from '@/repositories/TokenRepository'
+
+export class AuthRepository {
+  public static async getUser(): Promise<TAuth> {
+    return HttpServices.get({
+      url: 'user',
+    })
+  }
+
+  public static async login(params: TLoginParams): Promise<TAuth> {
+    const response = await HttpServices.post<TAuth>({
+      url: 'login',
+      data: params,
+    })
+
+    TokenRepository.set(response.token)
+
+    return response
+  }
+
+  public static async logout(): Promise<void> {
+    await HttpServices.get({
+      url: 'logout',
+    })
+
+    TokenRepository.delete()
+  }
+
+  public static async recoverAccount(
+    params: TRecoverAccountParams,
+  ): Promise<void> {
+    await HttpServices.post({
+      url: 'recover',
+      data: params,
+    })
+  }
+
+  public static async updatePassword(
+    params: TUpdatePasswordParams,
+  ): Promise<void> {
+    await HttpServices.post({
+      url: 'updatepass',
+      data: params,
+    })
+  }
+}
