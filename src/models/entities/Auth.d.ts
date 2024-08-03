dfa39d1bb28444f4631055162898f12e50804c22
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-03 20:53:33 -0300

    feat: Add Auth entity type for user authentication

---
+++ src/models/entities/Auth.d.ts
@@ -0,0 +1,25 @@
+type TFile = {
+  filename: string
+  extension: string
+  file: string
+  logo: null
+}
+
+type TAuthPermission = {
+  id: string
+  idpermission: string
+  name: string
+}
+
+type TAuth = {
+  iduser: number
+  idclient: number
+  login: string
+  name: string
+  email: string
+  phone: string
+  token: string
+  lastlogin: string
+  permissions: Array<TAuthPermission>
+  client_logo_icon: TFile
+}
