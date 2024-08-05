b310d23b205f7a9b37bd6cad88f4cef8540a1592
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-05 18:45:50 -0300

    feat: Add ClientRepository for managing client data

---
+++ src/repositories/ClientRepository.ts
@@ -0,0 +1,33 @@
+import { BaseRepository } from './shared/BaseRepository'
+
+export class ClientRepository extends BaseRepository {
+  public static async getClients(
+    params: TGetClientsParams,
+  ): Promise<Array<TClient>> {
+    return super.getAll({
+      url: 'Clients',
+      params,
+    })
+  }
+
+  public static async createClient(params: TCreateClientParams): Promise<void> {
+    return super.create({
+      url: 'addClient',
+      data: params,
+    })
+  }
+
+  public static async updateClient(params: TUpdateClientParams): Promise<void> {
+    await super.create({
+      url: 'addClient',
+      data: params,
+    })
+  }
+
+  public static async deleteClient(params: TDeleteClientParams): Promise<void> {
+    await super.delete({
+      url: 'delClient',
+      params,
+    })
+  }
+}
