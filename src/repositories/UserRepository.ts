435feada52d51ca2a4c168acf72e258dc25e688e
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-05 18:45:54 -0300

    feat: Add UserRepository for managing user data

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,31 @@
+import { BaseRepository } from './shared/BaseRepository'
+
+export class UserRepository extends BaseRepository {
+  public static async getUsers(params: TGetUsersParams): Promise<Array<TUser>> {
+    return super.getAll({
+      url: 'users',
+      params,
+    })
+  }
+
+  public static async createUser(params: TCreateUserParams): Promise<void> {
+    return super.create({
+      url: 'adduser',
+      data: params,
+    })
+  }
+
+  public static async updateUser(params: TUpdateUserParams): Promise<void> {
+    await super.create({
+      url: 'adduser',
+      data: params,
+    })
+  }
+
+  public static async deleteUser(params: TDeleteUserParams): Promise<void> {
+    await super.delete({
+      url: 'deluser',
+      params,
+    })
+  }
+}
