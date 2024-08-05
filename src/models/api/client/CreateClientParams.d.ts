4f1f5629393e6d154522a5ac74fe530e02d1b85a
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-05 18:44:46 -0300

    feat: Add CreateClientParams type for creating a new client

---
+++ src/models/api/client/CreateClientParams.d.ts
@@ -0,0 +1,13 @@
+type TCreateClientParams = {
+  name: string
+  cpnj: string
+  razaosocial: string
+  rua: string
+  numero: string
+  complemento: string
+  cep: string
+  bairro: string
+  cidade: string
+  estado: string
+  ativo: string
+}
