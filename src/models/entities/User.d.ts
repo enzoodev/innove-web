8b12f13a844e789194b373a5c0655611d8cbd576
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-05 18:45:43 -0300

    feat: Add User type for representing a user entity

---
+++ src/models/entities/User.d.ts
@@ -0,0 +1,11 @@
+type TUser = {
+  iduser: number
+  datecreate: string
+  createdby: string
+  name: string
+  email: string
+  phone: string
+  login: string
+  datelastlogin: string
+  statususer: string
+}
