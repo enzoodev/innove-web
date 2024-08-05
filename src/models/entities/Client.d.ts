cc121468923e4002f03773bf7fe10e903a8dd676
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-05 18:45:40 -0300

    feat: Add Client type for representing a client entity

---
+++ src/models/entities/Client.d.ts
@@ -0,0 +1,10 @@
+type TClient = {
+  idclient: number
+  datecreate: string
+  name: string
+  cnpj: string
+  razaosocial: string
+  datelasfmodify: string
+  status: string
+  address: Array<TAddress>
+}
