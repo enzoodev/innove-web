858e8eed79f033b0db1d519383a7e4227c6f0a89
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-07 17:41:45 -0300

    feat: Add CreateConstructionParams type for creating new constructions

---
+++ src/models/api/locations/CreateConstructionParams.d.ts
@@ -0,0 +1,17 @@
+type TCreateConstructionParams = {
+  idclient: string
+  nome: string
+  cnpj: string
+  razaosocial: string
+  datastart: string
+  rua: string
+  numero: string
+  complemento: string
+  cep: string
+  bairro: string
+  cidade: string
+  estado: string
+  responsavelnome: string
+  responsavelemail: string
+  ativo: string
+}
