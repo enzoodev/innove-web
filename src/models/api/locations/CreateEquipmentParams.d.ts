e00d45dbde0c5bc9906331633b3f2b6b82578c9f
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-07 17:41:49 -0300

    feat: Add CreateEquipmentParams type for creating new equipment

---
+++ src/models/api/locations/CreateEquipmentParams.d.ts
@@ -0,0 +1,10 @@
+type TCreateEquipmentParams = {
+  idclient: string
+  tipoequipamento: string
+  modelo: string
+  placa: string
+  tag: string
+  responsavelnome: string
+  responsavelemail: string
+  ativo: string
+}
