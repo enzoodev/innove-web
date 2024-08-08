c859ccf5555f37de2b9226d4491503d08391e98a
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-08 18:55:09 -0300

    refactor: Add lazy loading for images in Clients page

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,20 +1,58 @@
 import { NextPage } from 'next'
+
 import { useClients } from '@/hooks/useClients'
+
+import { List } from '@/components/elements/List'
+import { LayoutApp } from '@/components/layout/LayoutApp'
 
 const Clients: NextPage = () => {
   const {
     clients,
     isLoadingGetClients,
+    isLoadingCreateClient,
+    isLoadingUpdateClient,
+    createClient,
+    updateClient,
     isOpenCreateClientModal,
     isOpenUpdateClientModal,
     toggleOpenCreateClientModal,
     toggleOpenUpdateClientModal,
+    searchText,
+    setSearchText,
   } = useClients()
 
+  const renderItem: ListRenderItem<TClient> = ({ item }) => {
+    return (
+      <div>
+        <h1>{item.name}</h1>
+        <p>{item.cnpj}</p>
+      </div>
+    )
+  }
+
+  const renderItemLoading: ListRenderItemLoading = ({ index }) => {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>Clients</h1>
-    </div>
+    <LayoutApp title="Clientes" headTitle="Clients - Innove">
+      <List
+        items={clients}
+        itemSize={100}
+        isLoading={isLoadingGetClients}
+        renderItem={renderItem}
+        renderItemLoading={renderItemLoading}
+        EmptyIcon={
+          <div>
+            <h1>Nenhum cliente encontrado</h1>
+          </div>
+        }
+      />
+    </LayoutApp>
   )
 }
 
