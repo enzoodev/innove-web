472b73a67dc1def009f51e6214b52695725bcc24
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-08 23:37:44 -0300

    feat: Add routes for clients, locations, checklists, and config

--- src/enums/Routes.ts
+++ src/enums/Routes.ts
@@ -1,5 +1,9 @@
 export enum Routes {
   LOGIN = '/auth/login',
   RECOVER_ACCOUNT = '/auth/recover-account',
+  CLIENTS = '/clients',
   USERS = '/users',
+  LOCATIONS = '/locations',
+  CHECKLISTS = '/checklists',
+  CONFIG = '/config',
 }
