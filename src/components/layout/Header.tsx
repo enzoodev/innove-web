1f96c461f22ad99258bf7ddae038930382147bc6
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-10 23:02:40 -0300

    refactor: Update Header component to improve responsiveness and add shadow to button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,17 +18,17 @@ export const Header: React.NamedExoticComponent<Props> = memo(
     )
 
     return (
-      <header className="flex flex-row items-center justify-between mb-4">
+      <header className="flex flex-col sm:flex-row sm:items-center justify-between gap-4 mb-4">
         <div className="flex flex-row items-center gap-4">
           <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
           <button
             type="button"
-            className="w-12 h-12 flex items-center justify-center rounded-full bg-cyan-800 hover:bg-cyan-900 active:bg-cyan-950"
+            className="w-12 h-12 flex items-center justify-center rounded-full bg-cyan-800 hover:bg-cyan-900 active:bg-cyan-950 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
           >
             <IconPlus stroke={1.75} className="w-7 h-7 text-white" />
           </button>
         </div>
-        <div className="w-96">
+        <div className="w-full sm:w-72 lg:w-96">
           <Input
             placeholder="Qual cliente você procura?"
             hasLabel={false}
