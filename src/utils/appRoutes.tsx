db54660543ab6b4dcc668bcb7783986e9f468a39
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-10 23:04:28 -0300

    refactor: Update appRoutes to include labels for each route

--- src/utils/appRoutes.tsx
+++ src/utils/appRoutes.tsx
@@ -10,18 +10,22 @@ import { Routes } from '@/enums/Routes'
 export const appRoutes = [
   {
     name: Routes.CLIENTS,
+    label: 'Clientes',
     renderIcon: (props: IconProps) => <IconUserCircle {...props} />,
   },
   {
     name: Routes.USERS,
+    label: 'Usuários',
     renderIcon: (props: IconProps) => <IconUsers {...props} />,
   },
   {
     name: Routes.LOCATIONS,
+    label: 'Inspeções',
     renderIcon: (props: IconProps) => <IconHammer {...props} />,
   },
   {
     name: Routes.CHECKLISTS,
+    label: 'Checklists',
     renderIcon: (props: IconProps) => <IconChecklist {...props} />,
   },
 ]
