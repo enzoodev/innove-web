04ff941ca64f81ea043a63e0b54fecf7d49ce795
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-13 17:57:31 -0300

    feat: Add new query keys for retrieving checklists and users

--- src/enums/QueryKey.ts
+++ src/enums/QueryKey.ts
@@ -6,4 +6,8 @@ export enum QueryKey {
   GET_AREA_BY_ID = 'get-area-by-id',
   GET_CONSTRUCTION_BY_ID = 'get-construction-by-id',
   GET_EQUIPMENT_BY_ID = 'get-equipment-by-id',
+  GET_CHECKLISTS = 'get-checklists',
+  GET_CHECKLIST_BY_ID = 'get-checklist-by-id',
+  GET_USERS = 'get-users',
+  GET_USER_BY_ID = 'get-user-by-id',
 }
