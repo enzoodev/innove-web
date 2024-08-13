8627f5f5e82ac6ecef1d10065fb78c9de473d7de
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-13 13:43:55 -0300

    refactor: Add QueryKey enum for defining query keys

---
+++ src/enums/QueryKey.ts
@@ -0,0 +1,9 @@
+export enum QueryKey {
+  GET_USER = 'get-user',
+  GET_CLIENTS = 'get-clients',
+  GET_CLIENT_BY_ID = 'get-client-by-id',
+  GET_LOCATIONS = 'get-locations',
+  GET_AREA_BY_ID = 'get-area-by-id',
+  GET_CONSTRUCTION_BY_ID = 'get-construction-by-id',
+  GET_EQUIPMENT_BY_ID = 'get-equipment-by-id',
+}
