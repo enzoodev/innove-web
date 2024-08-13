d95cae614f63ea5c6e5b8da3a3e0c5b2314b185a
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-13 17:58:34 -0300

    refactor: Add ChecklistRepository implementation for checklist data retrieval and manipulation

---
+++ src/infrastructure/repositories/ChecklistRepository.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-useless-constructor */
+
+export class ChecklistRepository implements IChecklistRepository {
+  constructor(private readonly baseRepository: IBaseRepository) {}
+
+  public async getAll(): Promise<Array<TChecklist>> {
+    return this.baseRepository.getAll({
+      url: 'checklist',
+    })
+  }
+
+  public async getById(params: TGetChecklistParams): Promise<TChecklist> {
+    return this.baseRepository.get<TChecklist>({
+      url: 'checklist',
+      params,
+    })
+  }
+
+  public async create(params: TCreateChecklistParams): Promise<void> {
+    await this.baseRepository.create({
+      url: 'addchecklist',
+      data: params,
+    })
+  }
+
+  public async update(params: TUpdateChecklistParams): Promise<void> {
+    await this.baseRepository.create({
+      url: 'addchecklist',
+      data: params,
+    })
+  }
+
+  public async inactiveQuestion(
+    params: TInactiveChecklistQuestionParams,
+  ): Promise<void> {
+    await this.baseRepository.delete({
+      url: 'delquestion',
+      params,
+    })
+  }
+}
