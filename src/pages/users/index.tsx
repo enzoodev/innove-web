a97c05e1ebaf69ad536144cf9be55ce2a70e31c1
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-18 19:35:35 -0300

    refactor: Add lazy loading for images in Users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,57 +1,123 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { NextPage } from 'next'
+import { IconUserOff } from '@tabler/icons-react'
 
+import { useToggle } from '@/hooks/shared/useToggle'
 import { useGetUsers } from '@/hooks/user/useGetUsers'
 
 import { ListSeparators } from '@/utils/ListSeparators'
 
 import { LayoutApp } from '@/components/layout/LayoutApp'
+import { Input } from '@/components/elements/Input'
+import { ListEmpty } from '@/components/elements/ListEmpty'
+import { UserItem } from '@/components/modules/users/UserItem'
+import { UserSkeletonItem } from '@/components/modules/users/UserSkeletonItem'
+import { DeleteUserModal } from '@/components/modules/users/DeleteUserModal'
 
 const Users: NextPage = () => {
   const { users, isLoadingGetUsers, searchText, setSearchText } = useGetUsers()
 
+  const [isOpenUpdateModal, toggleOpenUpdateModal] = useToggle()
+  const [isOpenDeleteModal, toggleOpenDeleteModal] = useToggle()
+  const [user, setUser] = useState({
+    id: 0,
+    name: '',
+  })
+
+  const handleUpdate = useCallback(
+    (user: TUser) => {
+      setUser({
+        id: user.iduser,
+        name: user.name,
+      })
+      toggleOpenUpdateModal()
+    },
+    [toggleOpenUpdateModal],
+  )
+
+  const handleDelete = useCallback(
+    (user: TUser) => {
+      setUser({
+        id: user.iduser,
+        name: user.name,
+      })
+      toggleOpenDeleteModal()
+    },
+    [toggleOpenDeleteModal],
+  )
+
   const renderItems = useCallback(() => {
     if (isLoadingGetUsers) {
-      return Array.from({ length: 10 }).map((_, index, array) => {
-        const hasSeparator = ListSeparators.getHasSeparator(index, array)
-        return (
-          <div key={index}>
-            {/* <UserSkeletonItem /> */}
-            {hasSeparator && <hr className="border-t border-gray-300" />}
-          </div>
-        )
-      })
+      return (
+        <div className="bg-gray-100 rounded-md border border-gray-300 shadow-[0_3px_10px_rgb(0,0,0,0.1)]">
+          {Array.from({ length: 6 }).map((_, index, array) => {
+            const hasSeparator = ListSeparators.getHasSeparator(index, array)
+            return (
+              <div key={index}>
+                <UserSkeletonItem />
+                {hasSeparator && <hr className="border-t border-gray-300" />}
+              </div>
+            )
+          })}
+        </div>
+      )
     }
 
     if (users.length === 0) {
       return (
-        <div>
-          <h1>Nenhum usuário encontrado</h1>
-        </div>
+        <ListEmpty
+          renderIcon={() => (
+            <IconUserOff stroke={1.5} className="w-7 h-7 text-gray-700" />
+          )}
+          title="Nenhum usuário encontrado."
+        />
       )
     }
 
-    return users.map((item, index, array) => {
-      const hasSeparator = ListSeparators.getHasSeparator(index, array)
-      return (
-        <div key={item.iduser}>
-          {/* <UserItem item={item} /> */}
-          {hasSeparator && <hr className="border-t border-gray-300" />}
-        </div>
-      )
-    })
-  }, [users, isLoadingGetUsers])
+    return (
+      <div className="bg-gray-100 rounded-md border border-gray-300 shadow-[0_3px_10px_rgb(0,0,0,0.1)]">
+        {users.map((item, index, array) => {
+          const hasSeparator = ListSeparators.getHasSeparator(index, array)
+          return (
+            <div key={item.iduser}>
+              <UserItem
+                item={item}
+                onUpdate={() => handleUpdate(item)}
+                onDelete={() => handleDelete(item)}
+              />
+              {hasSeparator && <hr className="border-t border-gray-300" />}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }, [users, handleDelete, handleUpdate, isLoadingGetUsers])
 
   return (
     <LayoutApp
       title="Usuários"
       headTitle="Usuários - Innove"
-      searchText={searchText}
-      setSearchText={setSearchText}
+      headerRightComponent={
+        <div className="w-full sm:w-72 lg:w-96">
+          <Input
+            placeholder="Qual usuário você procura?"
+            hasLabel={false}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            aria-label="Search"
+            autoFocus
+            spellCheck={false}
+            additionalClasses="bg-gray-300 placeholder-gray-500"
+          />
+        </div>
+      }
     >
-      <div className="bg-gray-100 rounded-md border border-gray-300 shadow-[0_3px_10px_rgb(0,0,0,0.1)]">
-        {renderItems()}
-      </div>
+      {renderItems()}
+      <DeleteUserModal
+        item={user}
+        isOpen={isOpenDeleteModal}
+        onClose={toggleOpenDeleteModal}
+      />
     </LayoutApp>
   )
 }
