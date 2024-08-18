98ba475b25bd9f4da069db03a60c014932deb0d1
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-18 19:34:22 -0300

    refactor: Add DeleteUserModal component for deleting users

---
+++ src/components/modules/users/DeleteUserModal.tsx
@@ -0,0 +1,55 @@
+import React, { Fragment, memo, useCallback } from 'react'
+
+import { useDeleteUser } from '@/hooks/user/useDeleteUser'
+
+import { Modal } from '@/components/layout/Modal'
+import { Button } from '@/components/elements/Button'
+
+type Props = {
+  isOpen: boolean
+  item: {
+    name: string
+    id: number
+  }
+  onClose: () => void
+}
+
+export const DeleteUserModal: React.NamedExoticComponent<Props> = memo(
+  function Component({ isOpen, item, onClose }) {
+    const { deleteUser, isLoadingDeleteUser } = useDeleteUser(item.id)
+
+    const handleDelete = useCallback(async () => {
+      await deleteUser()
+      onClose()
+    }, [deleteUser, onClose])
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        title="Excluir usuário"
+        footer={
+          <Fragment>
+            <Button
+              onClick={onClose}
+              title="Cancelar"
+              itsCancelButton
+              additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
+              color="text-gray-700"
+            />
+            <Button
+              onClick={handleDelete}
+              title="Confirmar"
+              additionalClasses="w-36 bg-red-700 hover:bg-red-800 active:bg-red-900"
+              isLoading={isLoadingDeleteUser}
+            />
+          </Fragment>
+        }
+      >
+        <p className="text-sm text-gray-600">
+          Você tem certeza que deseja excluir o usuário <b>{item.name}</b> ?
+        </p>
+      </Modal>
+    )
+  },
+)
