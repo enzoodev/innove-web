cd9f309d24a6be34d0834cfc39c6beb3809cc07c
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-18 10:10:00 -0300

    refactor: Update SidebarModal component to improve layout and responsiveness

--- src/components/layout/LayoutApp.tsx
+++ src/components/layout/LayoutApp.tsx
@@ -3,8 +3,11 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { IconMenu2, IconSettings } from '@tabler/icons-react'
 
 import { useAuth } from '@/hooks/auth/useAuth'
+import { useToggle } from '@/hooks/shared/useToggle'
+
 import { Routes } from '@/enums/Routes'
 
 import { appRoutes } from '@/utils/appRoutes'
@@ -12,8 +15,6 @@ import { PhotoFormatter } from '@/utils/PhotoFormatter'
 
 import { Header } from './Header'
 import { Footer } from './Footer'
-import { IconMenu2, IconSettings } from '@tabler/icons-react'
-import { useToggle } from '@/hooks/shared/useToggle'
 import { SidebarModal } from './SiderbarModal'
 
 type Props = {
@@ -49,21 +50,17 @@ export const LayoutApp: React.NamedExoticComponent<Props> = memo(
           <title>{headTitle}</title>
         </Head>
         <div id="layout" className="flex flex-col md:hidden min-h-screen">
-          <div className="bg-gray-100 border-b border-gray-300 px-4 py-2 flex items-center justify-center relative">
-            <button
-              className="absolute bottom-3.5 left-4"
-              onClick={toggleSidebar}
-            >
-              <IconMenu2 stroke={1.5} className="w-8 h-8 text-gray-700" />
+          <div className="bg-cyan-950 border-b border-gray-300 px-4 py-2 flex items-center justify-between">
+            <button onClick={toggleSidebar}>
+              <IconMenu2 stroke={1.5} className="w-8 h-8 text-white" />
             </button>
-            <Link href="/clients">
-              <Image
-                src="/img/brand/innove.svg"
-                alt="Innove"
-                width="130"
-                height="37"
-              />
-            </Link>
+            <Image
+              src={clientLogo}
+              alt="Client_logo"
+              width="36"
+              height="36"
+              className="rounded-full"
+            />
           </div>
           <SidebarModal isOpen={isSidebarOpen} closeModal={toggleSidebar} />
           <div className="flex flex-col min-h-screen h-screen w-full p-4 bg-gray-200 overflow-auto">
@@ -77,42 +74,63 @@ export const LayoutApp: React.NamedExoticComponent<Props> = memo(
           </div>
         </div>
         <div id="layout" className="hidden md:flex min-h-screen flex-row">
-          <div className="flex relative p-2 flex-col justify-between w-2/4 h-full bg-cyan-950 shadow-lg">
-            <div className="flex flex-col">
-              <button className="flex self-end p-2" onClick={toggleSidebar}>
+          <div
+            className={`flex flex-col justify-between min-h-full bg-cyan-950 shadow-lg transition-all duration-300 ${
+              isSidebarOpen ? 'w-72 p-2' : 'w-20 p-4'
+            }`}
+          >
+            <div
+              className={`flex flex-col gap-4 ${isSidebarOpen ? '' : 'items-center'}`}
+            >
+              <button
+                className={isSidebarOpen ? 'self-end mt-2 mr-2' : ''}
+                onClick={toggleSidebar}
+              >
                 <IconMenu2 stroke={1.75} className="w-7 h-7 text-white" />
               </button>
-              <div className="flex flex-col items-center gap-4 my-4">
+              <div className="flex flex-col items-center gap-4">
                 <Image
                   src={clientLogo}
                   alt="Client_logo"
-                  width="72"
-                  height="72"
+                  width={isSidebarOpen ? '92' : '42'}
+                  height={isSidebarOpen ? '92' : '42'}
                   className="rounded-full"
                 />
-                <div className="flex flex-col items-center">
-                  <span className="text-lg font-semibold text-white break-words">
+                <div
+                  className={`flex-col items-center ${
+                    isSidebarOpen ? 'flex' : 'hidden'
+                  }`}
+                >
+                  <span className="text-md font-semibold text-white break-words">
                     {firstName} {secondName}
                   </span>
-                  <span className="text-sm font-medium text-gray-400 break-words">
+                  <span className="text-xs font-semibold text-gray-400 break-words">
                     {auth.email}
                   </span>
                 </div>
               </div>
-              <span className="mt-10 mb-3 ml-2 text-md font-semibold text-gray-100">
+            </div>
+            <div className="flex flex-col gap-3">
+              <span
+                className={`ml-2 text-sm font-semibold text-gray-100 ${
+                  isSidebarOpen ? 'inline-block' : 'hidden'
+                }`}
+              >
                 Navegação
               </span>
               <nav>
-                <ul className="flex flex-col gap-3">
+                <ul className="flex flex-col gap-2">
                   {appRoutes.map((item) => (
                     <Link href={item.name} key={item.name}>
                       <li
-                        className={`h-12 w-full flex items-center gap-2 pl-4 pr-2 rounded cursor-pointer
-                  ${
-                    router.pathname === item.name
-                      ? ' bg-gray-400 bg-opacity-20 active:bg-opacity-40'
-                      : 'hover:bg-gray-400 active:bg-gray-400 hover:bg-opacity-10 active:bg-opacity-40'
-                  }`}
+                        className={`h-12 w-full flex items-center gap-2 ${
+                          isSidebarOpen ? 'pl-4 pr-2 rounded' : 'p-3 rounded-xl'
+                        } cursor-pointer
+                        ${
+                          router.pathname === item.name
+                            ? ' bg-gray-400 bg-opacity-20 active:bg-opacity-40'
+                            : 'hover:bg-gray-400 active:bg-gray-400 hover:bg-opacity-10 active:bg-opacity-40'
+                        }`}
                         onClick={toggleSidebar}
                       >
                         {item.renderIcon({
@@ -124,7 +142,13 @@ export const LayoutApp: React.NamedExoticComponent<Props> = memo(
                           stroke: 1.5,
                         })}
                         <span
-                          className={`text-md font-medium ${router.pathname === item.name ? 'text-white' : 'text-gray-300'}`}
+                          className={`text-sm font-semibold ${
+                            isSidebarOpen ? 'inline-block' : 'hidden'
+                          } ${
+                            router.pathname === item.name
+                              ? 'text-white'
+                              : 'text-gray-300'
+                          }`}
                         >
                           {item.label}
                         </span>
@@ -134,26 +158,38 @@ export const LayoutApp: React.NamedExoticComponent<Props> = memo(
                 </ul>
               </nav>
             </div>
-            <Link href={Routes.CONFIG}>
-              <div
-                className={`h-11 w-full flex items-center gap-2 px-2 rounded cursor-pointer
-                  ${
-                    router.pathname === Routes.CONFIG
-                      ? ' bg-gray-400 bg-opacity-20 active:bg-opacity-40'
-                      : 'hover:bg-gray-400 active:bg-gray-400 hover:bg-opacity-10 active:bg-opacity-40'
-                  }`}
+            <Link href={Routes.CONFIG} key={Routes.CONFIG}>
+              <li
+                className={`h-12 w-full flex items-center gap-2 ${
+                  isSidebarOpen ? 'pl-4 pr-2 rounded' : 'p-3 rounded-xl'
+                } cursor-pointer
+                        ${
+                          router.pathname === Routes.CONFIG
+                            ? ' bg-gray-400 bg-opacity-20 active:bg-opacity-40'
+                            : 'hover:bg-gray-400 active:bg-gray-400 hover:bg-opacity-10 active:bg-opacity-40'
+                        }`}
                 onClick={toggleSidebar}
               >
                 <IconSettings
+                  className={`w-7 h-7 ${
+                    router.pathname === Routes.CONFIG
+                      ? 'text-white'
+                      : 'text-gray-300'
+                  }`}
                   stroke={1.5}
-                  className={`w-7 h-7 ${router.pathname === Routes.CONFIG ? 'text-white' : 'text-gray-300'}`}
                 />
                 <span
-                  className={`text-md font-medium ${router.pathname === Routes.CONFIG ? 'text-white' : 'text-gray-300'}`}
+                  className={`text-sm font-semibold ${
+                    isSidebarOpen ? 'inline-block' : 'hidden'
+                  } ${
+                    router.pathname === Routes.CONFIG
+                      ? 'text-white'
+                      : 'text-gray-300'
+                  }`}
                 >
                   Configurações
                 </span>
-              </div>
+              </li>
             </Link>
           </div>
           <div className="flex flex-col min-h-screen h-screen w-full p-4 bg-gray-200 overflow-auto">
