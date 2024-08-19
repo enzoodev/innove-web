79bc23a69ca8219e720bbc623e69700a9f89d016
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 12:18:25 -0300

    refactor: Update CreateClientParams type to include data and formData properties

--- src/domain/api/client/CreateClientParams.d.ts
+++ src/domain/api/client/CreateClientParams.d.ts
@@ -1,13 +1,4 @@
 type TCreateClientParams = {
-  name: string
-  cpnj: string
-  razaosocial: string
-  rua: string
-  numero: string
-  complemento: string
-  cep: string
-  bairro: string
-  cidade: string
-  estado: string
-  ativo: string
+  data: TSaveClient
+  formData: FormData
 }
