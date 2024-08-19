565f946253cd5feb037530c7b0baf6f832ca0bb1
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 14:48:34 -0300

    feat: Add saveUserSchema for validating user data

---
+++ src/schemas/user/saveUser.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const saveUserSchema = z.object({
+  nome: z.string().min(1, 'O nome é obrigatório.'),
+  telefone: z
+    .string()
+    .min(1, 'O telefone é obrigatório.')
+    .regex(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, 'Telefone inválido.'),
+  email: z.string().min(1, 'Informe o email.').email('Email inválido.'),
+  ativo: z.boolean(),
+  permission: z.array(z.number()).nonempty('Conceda ao menos uma permissão.'),
+})
+
+export type TSaveUserSchema = z.infer<typeof saveUserSchema>
