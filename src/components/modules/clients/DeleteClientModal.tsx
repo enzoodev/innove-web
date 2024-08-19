3f7a2c7cdb7ec5851844f385d634e2915008a9b9
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 12:18:12 -0300

    refactor: Remove unused import in DeleteClientModal component

--- src/components/modules/clients/DeleteClientModal.tsx
+++ src/components/modules/clients/DeleteClientModal.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, memo, useCallback } from 'react'
+import React, { Fragment, useCallback } from 'react'
 
 import { useDeleteClient } from '@/hooks/client/useDeleteClient'
 
@@ -14,42 +14,44 @@ type Props = {
   onClose: () => void
 }
 
-export const DeleteClientModal: React.NamedExoticComponent<Props> = memo(
-  function Component({ isOpen, item, onClose }) {
-    const { deleteClient, isLoadingDeleteClient } = useDeleteClient(item.id)
+export const DeleteClientModal: React.FC<Props> = ({
+  isOpen,
+  item,
+  onClose,
+}) => {
+  const { deleteClient, isLoadingDeleteClient } = useDeleteClient(item.id)
 
-    const handleDelete = useCallback(async () => {
-      await deleteClient()
-      onClose()
-    }, [deleteClient, onClose])
+  const handleDelete = useCallback(async () => {
+    await deleteClient()
+    onClose()
+  }, [deleteClient, onClose])
 
-    return (
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        title="Excluir cliente"
-        footer={
-          <Fragment>
-            <Button
-              onClick={onClose}
-              title="Cancelar"
-              itsCancelButton
-              additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
-              color="text-gray-700"
-            />
-            <Button
-              onClick={handleDelete}
-              title="Confirmar"
-              additionalClasses="w-36 bg-red-700 hover:bg-red-800 active:bg-red-900"
-              isLoading={isLoadingDeleteClient}
-            />
-          </Fragment>
-        }
-      >
-        <p className="text-sm text-gray-600">
-          Você tem certeza que deseja excluir o cliente <b>{item.name}</b> ?
-        </p>
-      </Modal>
-    )
-  },
-)
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Excluir cliente"
+      footer={
+        <Fragment>
+          <Button
+            onClick={onClose}
+            title="Cancelar"
+            itsCancelButton
+            additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
+            color="text-gray-700"
+          />
+          <Button
+            onClick={handleDelete}
+            title="Confirmar"
+            additionalClasses="w-36 bg-red-700 hover:bg-red-800 active:bg-red-900"
+            isLoading={isLoadingDeleteClient}
+          />
+        </Fragment>
+      }
+    >
+      <p className="text-sm text-gray-600">
+        Você tem certeza que deseja excluir o cliente <b>{item.name}</b> ?
+      </p>
+    </Modal>
+  )
+}
