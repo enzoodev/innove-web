eb4934548a66d5b3a2b30fc113e1d047130b11ed
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 12:18:16 -0300

    refactor: Remove unused import in DeleteUserModal component

--- src/components/modules/users/DeleteUserModal.tsx
+++ src/components/modules/users/DeleteUserModal.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, memo, useCallback } from 'react'
+import React, { Fragment, useCallback } from 'react'
 
 import { useDeleteUser } from '@/hooks/user/useDeleteUser'
 
@@ -14,42 +14,40 @@ type Props = {
   onClose: () => void
 }
 
-export const DeleteUserModal: React.NamedExoticComponent<Props> = memo(
-  function Component({ isOpen, item, onClose }) {
-    const { deleteUser, isLoadingDeleteUser } = useDeleteUser(item.id)
+export const DeleteUserModal: React.FC<Props> = ({ isOpen, item, onClose }) => {
+  const { deleteUser, isLoadingDeleteUser } = useDeleteUser(item.id)
 
-    const handleDelete = useCallback(async () => {
-      await deleteUser()
-      onClose()
-    }, [deleteUser, onClose])
+  const handleDelete = useCallback(async () => {
+    await deleteUser()
+    onClose()
+  }, [deleteUser, onClose])
 
-    return (
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        title="Excluir usuário"
-        footer={
-          <Fragment>
-            <Button
-              onClick={onClose}
-              title="Cancelar"
-              itsCancelButton
-              additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
-              color="text-gray-700"
-            />
-            <Button
-              onClick={handleDelete}
-              title="Confirmar"
-              additionalClasses="w-36 bg-red-700 hover:bg-red-800 active:bg-red-900"
-              isLoading={isLoadingDeleteUser}
-            />
-          </Fragment>
-        }
-      >
-        <p className="text-sm text-gray-600">
-          Você tem certeza que deseja excluir o usuário <b>{item.name}</b> ?
-        </p>
-      </Modal>
-    )
-  },
-)
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Excluir usuário"
+      footer={
+        <Fragment>
+          <Button
+            onClick={onClose}
+            title="Cancelar"
+            itsCancelButton
+            additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
+            color="text-gray-700"
+          />
+          <Button
+            onClick={handleDelete}
+            title="Confirmar"
+            additionalClasses="w-36 bg-red-700 hover:bg-red-800 active:bg-red-900"
+            isLoading={isLoadingDeleteUser}
+          />
+        </Fragment>
+      }
+    >
+      <p className="text-sm text-gray-600">
+        Você tem certeza que deseja excluir o usuário <b>{item.name}</b> ?
+      </p>
+    </Modal>
+  )
+}
