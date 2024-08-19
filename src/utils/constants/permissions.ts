fc29ad215260b1c53a6d359e749cfd481831c4ad
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 15:04:18 -0300

    refactor: Add constants for user permissions

---
+++ src/utils/constants/permissions.ts
@@ -0,0 +1,14 @@
+export const permissions = [
+  {
+    idpermission: '1',
+    label: 'Acesso Web - Cliente',
+  },
+  {
+    idpermission: '2',
+    label: 'Acesso App - Cliente',
+  },
+  {
+    idpermission: '9',
+    label: 'Administrador - Owner',
+  },
+]
