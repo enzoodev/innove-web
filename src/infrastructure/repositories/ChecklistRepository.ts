cf45ff5a7b7b8b93325d4fde558c9ed256e88cbb
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-18 22:17:25 -0300

    refactor: Update ChecklistRepository to use mock data for getAll method

--- src/infrastructure/repositories/ChecklistRepository.ts
+++ src/infrastructure/repositories/ChecklistRepository.ts
@@ -1,12 +1,15 @@
 /* eslint-disable no-useless-constructor */
 
+import { getChecklistsMock } from '@/mocks/get-checklists-mock'
+
 export class ChecklistRepository implements IChecklistRepository {
   constructor(private readonly baseRepository: IBaseRepository) {}
 
   public async getAll(): Promise<Array<TChecklist>> {
-    return this.baseRepository.getAll({
-      url: 'checklist',
-    })
+    // return this.baseRepository.getAll({
+    //   url: 'checklist',
+    // })
+    return getChecklistsMock as any
   }
 
   public async getById(params: TGetChecklistParams): Promise<TChecklist> {
---
+++ src/mocks/get-checklists-mock.ts
@@ -0,0 +1,74 @@
+export const getChecklistsMock = [
+  {
+    idchecklist: '1',
+    name: 'Novo Checklist',
+    topics: [
+      {
+        subtitle: '1. ÁREA DE VIVÊNCIA (REFEITÓRIO, BANHEIRO E VESTIÁRIO)',
+        questions: [
+          {
+            idquestion: '2',
+            name: '1.2 Bebedouro aterrado eletricamente ou Ponto de Hidratação nas proximidades dos postos de trabalho (100 metros - horizontal /15 metros - vertical)?',
+            ativo: '0',
+          },
+          {
+            idquestion: '12',
+            name: '1.1 Número de cadeiras suficientes para cada trabalhador?',
+            ativo: '1',
+          },
+          {
+            idquestion: '13',
+            name: '1.2 Bebedouro aterrado eletricamente ou Ponto de Hidratação nas proximidades dos postos de trabalho (100 metros - horizontal /15 metros - vertical)?',
+            ativo: '0',
+          },
+        ],
+      },
+      {
+        subtitle:
+          '1.11 Refeitório, banheiro e vestiário em boas condições de higiene?',
+        questions: [
+          {
+            idquestion: '14',
+            name: '7',
+            ativo: '1',
+          },
+        ],
+      },
+      {
+        subtitle: '2. MÁQUINAS E EQUIPAMENTOS (BOBCAT E MINI CARREGADEIRA)',
+        questions: [
+          {
+            idquestion: '4',
+            name: '2.2 O Check list OB 49 (Inspeção/ Manutenção Semanal de BOBCAT) estão preenchidos?',
+            ativo: '1',
+          },
+          {
+            idquestion: '15',
+            name: '2.2 O Check list OB 49 (Inspeção/ Manutenção Semanal de BOBCAT) estão preenchidos?',
+            ativo: '0',
+          },
+        ],
+      },
+      {
+        subtitle: 'cuidados',
+        questions: [
+          {
+            idquestion: '1',
+            name: 'teste 2',
+            ativo: '0',
+          },
+        ],
+      },
+      {
+        subtitle: 'teste subtitle',
+        questions: [
+          {
+            idquestion: '3',
+            name: 'teste 41',
+            ativo: '0',
+          },
+        ],
+      },
+    ],
+  },
+]
