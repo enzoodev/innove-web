ef0f37dd32b13c0c07f3160c0073e2e9f523940e
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-19 12:18:50 -0300

    refactor: Update useUpdateClient hook to handle file attachments and form submission

--- src/hooks/client/useUpdateClient.ts
+++ src/hooks/client/useUpdateClient.ts
@@ -1,23 +1,33 @@
-import { useCallback } from 'react'
+/* eslint-disable camelcase */
+import { ChangeEventHandler, useCallback, useState } from 'react'
 import { toast } from 'react-toastify'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { ClientRepository } from '@/infrastructure/repositories/ClientRepository'
 import { BaseRepository } from '@/infrastructure/repositories/shared/BaseRepository'
 import { httpServicesFactory } from '@/infrastructure/factories/httpServicesFactory'
 
+import {
+  saveClientSchema,
+  TSaveClientSchema,
+} from '@/schemas/clients/saveClient'
+
 import { QueryKey } from '@/enums/QueryKey'
 
 import { UrlBuilder } from '@/utils/UrlBuilder'
+import { createFile } from '@/utils/createFile'
 
 const httpServices = httpServicesFactory()
 const baseRepository = new BaseRepository(httpServices, new UrlBuilder())
 const clientRepository = new ClientRepository(baseRepository)
 
 export const useUpdateClient = (clientId: number) => {
+  const [isLoadingFetchFiles, setIsLoadingFetchFiles] = useState(false)
   const queryClient = useQueryClient()
 
-  const { mutateAsync: getClientByIdFn, isPending: isLoadingClient } =
+  const { mutateAsync: getClientByIdFn, isPending: isLoadingGetClient } =
     useMutation({
       mutationKey: [QueryKey.GET_CLIENT_BY_ID, clientId],
       mutationFn: clientRepository.getById,
@@ -28,34 +38,154 @@ export const useUpdateClient = (clientId: number) => {
       mutationFn: clientRepository.update,
     })
 
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    setValue,
+    reset,
+  } = useForm<TSaveClientSchema>({
+    resolver: zodResolver(saveClientSchema),
+    defaultValues: {
+      name: '',
+      cpnj: '',
+      razaosocial: '',
+      rua: '',
+      numero: '',
+      complemento: '',
+      cep: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+      ativo: true,
+      file_icon: undefined,
+      file_logo: undefined,
+    },
+  })
+
+  const handleFileIcon: ChangeEventHandler<HTMLInputElement> = useCallback(
+    ({ target }) => {
+      if (!target.files) return
+      setValue('file_icon', target.files[0])
+    },
+    [setValue],
+  )
+
+  const handleFileLogo: ChangeEventHandler<HTMLInputElement> = useCallback(
+    ({ target }) => {
+      if (!target.files) return
+      setValue('file_logo', target.files[0])
+    },
+    [setValue],
+  )
+
+  const fetchClientFiles = useCallback(
+    async (files: TClientAttachments) => {
+      try {
+        setIsLoadingFetchFiles(true)
+
+        const promises: Array<Promise<File>> = []
+
+        Object.keys(files).forEach((key: string) => {
+          const fileKey = key as keyof TClientAttachments
+
+          if (!files[fileKey]) {
+            return
+          }
+
+          promises.push(
+            createFile({
+              base64String: files[fileKey].file,
+              fileName: files[fileKey].filename ?? `${fileKey}-${clientId}`,
+              fileType: files[fileKey].extension,
+            }),
+          )
+        })
+
+        const [icon, logo] = await Promise.all(promises)
+
+        return {
+          icon,
+          logo,
+        }
+      } finally {
+        setIsLoadingFetchFiles(false)
+      }
+    },
+    [clientId],
+  )
+
   const fetchClient = useCallback(async () => {
     try {
       const client = await getClientByIdFn({ idclient: clientId })
+      const { icon, logo } = await fetchClientFiles(client.anexo)
+      const [address] = client.address
+
+      reset({
+        name: client.name,
+        cpnj: client.cnpj,
+        razaosocial: client.razaosocial,
+        rua: address.rua,
+        numero: address.numero,
+        complemento: address.complemento ?? undefined,
+        cep: address.cep,
+        bairro: address.bairro,
+        cidade: address.cidade,
+        estado: address.estado,
+        ativo: client.status === '1',
+        file_icon: icon,
+        file_logo: logo,
+      })
     } catch (error) {
       toast.error('Não foi possível buscar os dados do cliente.')
     }
-  }, [clientId, getClientByIdFn])
+  }, [clientId, fetchClientFiles, getClientByIdFn, reset])
 
-  const updateClient = useCallback(
-    async (data: TUpdateClientParams) => {
+  const onSubmit: SubmitHandler<TSaveClientSchema> = useCallback(
+    async (data) => {
       try {
-        await updateClientFn(data)
+        const { file_icon, file_logo, ...dataToRequest } = data
+        const formData = new FormData()
+
+        if (file_icon) {
+          formData.append('file_icon', file_icon)
+        }
+
+        if (file_logo) {
+          formData.append('file_logo', file_logo)
+        }
+
+        await updateClientFn({
+          data: {
+            ...dataToRequest,
+            ativo: dataToRequest.ativo ? '1' : '0',
+            idclient: clientId.toString(),
+          },
+          formData,
+        })
         toast.success('Cliente atualizado com sucesso!')
         queryClient.invalidateQueries({ queryKey: [QueryKey.GET_CLIENTS] })
         queryClient.invalidateQueries({
           queryKey: [QueryKey.GET_CLIENT_BY_ID, clientId],
         })
+        reset()
       } catch (error) {
         toast.error('Não foi possível atualizar o cliente.')
       }
     },
-    [updateClientFn, queryClient, clientId],
+    [updateClientFn, clientId, queryClient, reset],
   )
+
+  const handleUpdateClient = handleSubmit(onSubmit)
 
   return {
     fetchClient,
-    updateClient,
-    isLoadingClient,
+    handleUpdateClient,
+    isLoadingClient: isLoadingGetClient || isLoadingFetchFiles,
     isLoadingUpdateClient,
+    register,
+    errors,
+    handleFileIcon,
+    handleFileLogo,
   }
 }
