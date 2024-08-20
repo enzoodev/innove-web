af2bb920c038fed80335e8e81201ce2ca14ca93b
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-20 16:28:20 -0300

    refactor: Update saveUserSchema to include permission validation

--- src/schemas/user/saveUserSchema.ts
+++ src/schemas/user/saveUserSchema.ts
@@ -1,14 +1,25 @@
 import { TypeOf, z } from 'zod'
 
-export const saveUserSchema = z.object({
-  nome: z.string().min(1, 'O nome é obrigatório.'),
-  telefone: z
-    .string()
-    .min(1, 'O telefone é obrigatório.')
-    .regex(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, 'Telefone inválido.'),
-  email: z.string().min(1, 'O email é obrigatório.').email('Email inválido.'),
-  ativo: z.boolean(),
-  permission: z.array(z.number()).nonempty('Conceda ao menos uma permissão.'),
-})
+export const saveUserSchema = z
+  .object({
+    nome: z.string().min(1, 'O nome é obrigatório.'),
+    telefone: z
+      .string()
+      .min(1, 'O telefone é obrigatório.')
+      .regex(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, 'Telefone inválido.'),
+    email: z.string().min(1, 'O email é obrigatório.').email('Email inválido.'),
+    ativo: z.boolean(),
+    permission: z.array(
+      z.object({
+        label: z.string(),
+        value: z.string(),
+        isActive: z.boolean(),
+      }),
+    ),
+  })
+  .refine((data) => data.permission.some((item) => item.isActive), {
+    message: 'Conceda ao menos uma permissão.',
+    path: ['permission'],
+  })
 
 export type TSaveUserSchema = TypeOf<typeof saveUserSchema>
