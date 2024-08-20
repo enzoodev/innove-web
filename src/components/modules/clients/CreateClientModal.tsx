99b9a842ef2c882345da16877b416adb0d71e0ef
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-19 22:07:57 -0300

    refactor: Update CreateClientModal component to include form fields for client information

--- src/components/modules/clients/CreateClientModal.tsx
+++ src/components/modules/clients/CreateClientModal.tsx
@@ -4,6 +4,7 @@ import { useCreateClient } from '@/hooks/client/useCreateClient'
 
 import { Modal } from '@/components/layout/Modal'
 import { Button } from '@/components/elements/Button'
+import { Input } from '@/components/elements/Input'
 
 type Props = {
   isOpen: boolean
@@ -24,28 +25,110 @@ export const CreateClientModal: React.FC<Props> = ({ isOpen, onClose }) => {
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      title="Criar cliente"
+      title="Cadastrar cliente"
+      onSubmit={handleCreateClient(onClose)}
+      dialogPanelClassName="max-w-2xl"
       footer={
         <Fragment>
           <Button
             title="Voltar"
             itsCancelButton
-            additionalClasses="bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
+            additionalClasses="w-36 bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
             color="text-gray-700"
             onClick={onClose}
           />
           <Button
             title="Salvar"
-            additionalClasses="w-36 bg-cyan-700 hover:bg-cyan-800 active:bg-cyan-900"
+            additionalClasses="w-44 bg-cyan-800 hover:bg-cyan-900 active:bg-cyan-950"
             isLoading={isLoadingCreateClient}
-            onClick={handleCreateClient}
+            type="submit"
           />
         </Fragment>
       }
     >
-      <p className="text-sm text-gray-600">
-        Você tem certeza que deseja excluir o cliente
-      </p>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+        <div className="col-span-1 sm:col-span-2">
+          <Input
+            placeholder={'Nome'}
+            formError={errors.name?.message}
+            name="name"
+            autoFocus
+            register={register}
+          />
+        </div>
+
+        <Input
+          placeholder={'CNPJ'}
+          formError={errors.cpnj?.message}
+          name="cpnj"
+          autoFocus
+          register={register}
+        />
+        <Input
+          placeholder={'Razão Social'}
+          formError={errors.razaosocial?.message}
+          name="razaosocial"
+          autoFocus
+          register={register}
+        />
+
+        <Input
+          placeholder={'Rua'}
+          formError={errors.rua?.message}
+          name="rua"
+          autoFocus
+          register={register}
+        />
+        <Input
+          placeholder={'Número'}
+          formError={errors.numero?.message}
+          name="numero"
+          autoFocus
+          register={register}
+        />
+        <div className="col-span-1 sm:col-span-2">
+          <Input
+            placeholder={'Complemento'}
+            formError={errors.complemento?.message}
+            name="complemento"
+            autoFocus
+            register={register}
+          />
+        </div>
+        <Input
+          placeholder={'Bairro'}
+          formError={errors.bairro?.message}
+          name="bairro"
+          autoFocus
+          register={register}
+        />
+        <Input
+          placeholder={'CEP'}
+          formError={errors.cep?.message}
+          name="cep"
+          autoFocus
+          register={register}
+        />
+        <Input
+          placeholder={'Cidade'}
+          formError={errors.cidade?.message}
+          name="cidade"
+          autoFocus
+          register={register}
+        />
+        <Input
+          placeholder={'Estado'}
+          formError={errors.estado?.message}
+          name="estado"
+          autoFocus
+          register={register}
+        />
+
+        <div className="col-span-1 sm:col-span-2 flex items-center">
+          <input type="checkbox" {...register('ativo')} />
+          <label className="ml-2">Ativo</label>
+        </div>
+      </div>
     </Modal>
   )
 }
