fa934dab05b9507b023577a5d544b0e9e54aa30d
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-21 15:15:31 -0300

    refactor: Update useCreateEquipment to include client ID in createEquipmentFn call

--- src/hooks/location/useCreateEquipment.ts
+++ src/hooks/location/useCreateEquipment.ts
@@ -1,12 +1,22 @@
 import { useCallback } from 'react'
 import { toast } from 'react-toastify'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useAuth } from '@/hooks/auth/useAuth'
+
+import {
+  saveEquipmentSchema,
+  TSaveEquipmentSchema,
+} from '@/schemas/location/saveEquipmentSchema'
 
 import { createEquipment } from '@/query/location/createEquipment'
 
 import { QueryKey } from '@/enums/QueryKey'
 
 export const useCreateEquipment = () => {
+  const { clientId } = useAuth()
   const queryClient = useQueryClient()
 
   const {
@@ -16,22 +26,62 @@ export const useCreateEquipment = () => {
     mutationFn: createEquipment,
   })
 
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    watch,
+    setValue,
+    reset,
+  } = useForm<TSaveEquipmentSchema>({
+    resolver: zodResolver(saveEquipmentSchema),
+    defaultValues: {
+      tipoequipamento: '',
+      modelo: '',
+      placa: '',
+      tag: '',
+      responsavelnome: '',
+      responsavelemail: '',
+      ativo: true,
+    },
+  })
+
+  const userIsActive = watch('ativo')
+
+  const handleUserActiveChange = useCallback(() => {
+    setValue('ativo', !userIsActive)
+  }, [setValue, userIsActive])
+
   const onSubmit = useCallback(
-    async (callback: () => void) => {
+    async (data: TSaveEquipmentSchema, callback: () => void) => {
       try {
-        await createEquipmentFn({})
+        await createEquipmentFn({
+          ...data,
+          idclient: clientId.toString(),
+          ativo: data.ativo ? '1' : '0',
+        })
         callback()
         toast.success('Inspeção cadastrada com sucesso!')
         queryClient.invalidateQueries({ queryKey: [QueryKey.GET_LOCATIONS] })
+        reset()
       } catch (error) {
         toast.error('Não foi possível cadastrar a inspeção.')
       }
     },
-    [createEquipmentFn, queryClient],
+    [clientId, createEquipmentFn, queryClient, reset],
+  )
+
+  const handleCreateEquipment = useCallback(
+    (callback: () => void) => handleSubmit((data) => onSubmit(data, callback)),
+    [handleSubmit, onSubmit],
   )
 
   return {
-    createEquipment: onSubmit,
+    handleCreateEquipment,
     isLoadingCreateEquipment,
+    register,
+    errors,
+    userIsActive,
+    handleUserActiveChange,
   }
 }
