aa7fd18716515073a939aaf8944aed91802f5dbe
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-21 15:15:54 -0300

    feat: Add location page component

---
+++ src/pages/location/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+import { LayoutApp } from '@/components/layout/LayoutApp'
+
+const Location: NextPage = () => {
+  return <LayoutApp></LayoutApp>
+}
+
+export default Location
