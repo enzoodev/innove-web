df0067df6be3ca544d616ac322c7f3a85d4a3e28
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-21 16:13:17 -0300

    refactor: Simplify PhotoFormatter formatUri method
    Simplify the formatUri method in the PhotoFormatter class by removing the unnecessary getFileType function and directly using the lowercase extension to determine the fileType. This improves code readability and reduces complexity.

--- src/utils/PhotoFormatter.ts
+++ src/utils/PhotoFormatter.ts
@@ -1,18 +1,8 @@
 export class PhotoFormatter {
-  public static readonly getFileType = (extension: string) => {
-    const lowerCaseExtension = extension.toLowerCase()
-
-    if (lowerCaseExtension === 'pdf') {
-      return `application/${lowerCaseExtension}`
-    }
-
-    return `image/${lowerCaseExtension}`
-  }
-
   public static readonly formatUri = (photo?: TFile) => {
     if (!photo) return ''
 
-    const fileType = this.getFileType(photo.extension)
+    const fileType = photo.extension.toLowerCase()
     return `data:${fileType};base64,${photo.file}`
   }
 }
