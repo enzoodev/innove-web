0dd8b906782f62035ae527d1c4b67672674d865b
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-21 15:15:39 -0300

    refactor: Update useUpdateConstruction to include client ID in getConstructionFn and updateConstructionFn calls

--- src/hooks/location/useUpdateConstruction.ts
+++ src/hooks/location/useUpdateConstruction.ts
@@ -1,18 +1,36 @@
 import { useCallback } from 'react'
 import { toast } from 'react-toastify'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useAuth } from '@/hooks/auth/useAuth'
+
+import {
+  saveConstructionSchema,
+  TSaveConstructionSchema,
+} from '@/schemas/location/saveConstructionSchema'
 
 import { getConstruction } from '@/query/location/getConstruction'
 import { updateConstruction } from '@/query/location/updateConstruction'
 
 import { QueryKey } from '@/enums/QueryKey'
 
-export const useUpdateConstruction = (params: TGetLocationParams) => {
+type TUseUpdateConstructionParams = {
+  locationId: number
+  locationTypeId: number
+}
+
+export const useUpdateConstruction = ({
+  locationId,
+  locationTypeId,
+}: TUseUpdateConstructionParams) => {
+  const { clientId } = useAuth()
   const queryClient = useQueryClient()
 
   const { mutateAsync: getConstructionFn, isPending: isLoadingConstruction } =
     useMutation({
-      mutationKey: [QueryKey.GET_CONSTRUCTION_BY_ID, params.idlocal],
+      mutationKey: [QueryKey.GET_CONSTRUCTION_BY_ID, locationId],
       mutationFn: getConstruction,
     })
 
@@ -23,35 +41,105 @@ export const useUpdateConstruction = (params: TGetLocationParams) => {
     mutationFn: updateConstruction,
   })
 
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    watch,
+    setValue,
+    reset,
+  } = useForm<TSaveConstructionSchema>({
+    resolver: zodResolver(saveConstructionSchema),
+    defaultValues: {
+      nome: '',
+      cnpj: '',
+      razaosocial: '',
+      datastart: '',
+      rua: '',
+      numero: '',
+      complemento: '',
+      cep: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+      responsavelnome: '',
+      responsavelemail: '',
+      ativo: true,
+    },
+  })
+
+  const userIsActive = watch('ativo')
+
+  const handleUserActiveChange = useCallback(() => {
+    setValue('ativo', !userIsActive)
+  }, [setValue, userIsActive])
+
   const fetchConstruction = useCallback(async () => {
     try {
-      const construction = await getConstructionFn(params)
+      const construction = await getConstructionFn({
+        idtipo: locationTypeId,
+        idlocal: locationId,
+        idclient: clientId,
+      })
+      const [address] = construction.address
+
+      reset({
+        nome: construction.nome,
+        cnpj: construction.cnpj,
+        razaosocial: construction.razaosocial,
+        datastart: construction.datestart,
+        rua: address.rua,
+        numero: address.numero,
+        complemento: address.complemento ?? '',
+        cep: address.cep,
+        bairro: address.bairro,
+        cidade: address.cidade,
+        estado: address.estado,
+        responsavelnome: construction.responsavelnome,
+        responsavelemail: construction.responsavelemail,
+        ativo: construction.status === '1',
+      })
     } catch (error) {
       toast.error('Não foi possível buscar os dados da inspeção.')
     }
-  }, [getConstructionFn, params])
+  }, [clientId, getConstructionFn, locationId, locationTypeId, reset])
 
   const onSubmit = useCallback(
-    async (callback: () => void) => {
+    async (data: TSaveConstructionSchema, callback: () => void) => {
       try {
-        await updateConstructionFn({})
+        await updateConstructionFn({
+          ...data,
+          idlocal: locationId.toString(),
+          idclient: clientId.toString(),
+          ativo: data.ativo ? '1' : '0',
+        })
         callback()
         toast.success('Inspeção editada com sucesso!')
         queryClient.invalidateQueries({ queryKey: [QueryKey.GET_LOCATIONS] })
         queryClient.invalidateQueries({
-          queryKey: [QueryKey.GET_CONSTRUCTION_BY_ID, params.idlocal],
+          queryKey: [QueryKey.GET_CONSTRUCTION_BY_ID, locationId],
         })
+        reset()
       } catch (error) {
         toast.error('Não foi possível editar a inspeção.')
       }
     },
-    [updateConstructionFn, queryClient, params.idlocal],
+    [updateConstructionFn, locationId, clientId, queryClient, reset],
+  )
+
+  const handleUpdateConstruction = useCallback(
+    (callback: () => void) => handleSubmit((data) => onSubmit(data, callback)),
+    [handleSubmit, onSubmit],
   )
 
   return {
     fetchConstruction,
-    updateConstruction: onSubmit,
+    handleUpdateConstruction,
     isLoadingConstruction,
     isLoadingUpdateConstruction,
+    register,
+    errors,
+    userIsActive,
+    handleUserActiveChange,
   }
 }
