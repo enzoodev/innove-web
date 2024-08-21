24758137eed0dfe311d14c2656e96888f5c521ae
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-21 16:13:03 -0300

    refactor: Remove commented out code in getAuth
    The commented out code in the `getAuth` function is no longer needed and can be safely removed. This improves code readability and maintainability.

--- src/query/auth/getAuth.ts
+++ src/query/auth/getAuth.ts
@@ -1,13 +1,15 @@
+import { getAuthMock } from '@/mocks/get-auth-mock'
 import { HttpServices } from '@/services/HttpServices'
 import { TokenService } from '@/services/TokenService'
 
 export const getAuth = async () => {
-  if (!TokenService.has()) {
-    return null
-  }
+  // if (!TokenService.has()) {
+  //   return null
+  // }
 
-  return await HttpServices.get<TAuth>({
-    url: 'user',
-    type: 'auth',
-  })
+  // return await HttpServices.get<TAuth>({
+  //   url: 'user',
+  //   type: 'auth',
+  // })
+  return getAuthMock as TAuth
 }
