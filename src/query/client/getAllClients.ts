7d05286c37e930064a514cbc32c68c9663ce50d4
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-22 23:12:38 -0300

    refactor: Remove console.log statement in getAllClients

--- src/query/client/getAllClients.ts
+++ src/query/client/getAllClients.ts
@@ -5,9 +5,5 @@ export const getAllClients = async () => {
   // return HttpServices.get<Array<TClient>>({
   //   url: 'clients',
   // })
-  const clients = await HttpServices.get<Array<TClient>>({
-    url: 'clients',
-  })
-  console.log('clients', clients)
   return getClientsMock as Array<TClient>
 }
