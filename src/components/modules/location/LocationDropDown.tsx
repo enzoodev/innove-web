4b3ab7afbef0d48f987b97190ad836f60525b195
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-22 22:50:09 -0300

    refactor: Update LocationDropDown component with improved styling and functionality

--- src/components/modules/location/LocationDropDown.tsx
+++ src/components/modules/location/LocationDropDown.tsx
@@ -1,3 +1,5 @@
+import { IconChevronDown } from '@tabler/icons-react'
+
 type Props = {
   title: string
   isOpen: boolean
@@ -12,14 +14,16 @@ export const LocationDropDown: React.FC<Props> = ({
   children,
 }) => {
   return (
-    <div
-      className={`transition-colors duration-300 ${isOpen ? 'bg-gray-100' : 'bg-gray-200'}`}
-    >
-      <div>
-        <h3>{title}</h3>
+    <div>
+      <div className="flex flex-row items-center justify-between py-2 px-4 border-b border-gray-300">
+        <h3 className="text-gray-800 text-md font-semibold">{title}</h3>
         {!isOpen && (
-          <button type="button" onClick={onOpen}>
-            Open
+          <button
+            type="button"
+            onClick={onOpen}
+            className="w-10 h-10 bg-gray-300 border border-gray-400 rounded-full flex items-center justify-center"
+          >
+            <IconChevronDown stroke={1.5} className="text-gray-700 w-7 h-7" />
           </button>
         )}
       </div>
