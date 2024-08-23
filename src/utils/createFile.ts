d2899028406f51b20389fb5e8f879c1b21347f8b
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-22 22:50:24 -0300

    refactor: Improve createFile function to handle base64 file conversion

--- src/utils/createFile.ts
+++ src/utils/createFile.ts
@@ -1,15 +1,36 @@
+import { PhotoFormatter } from './PhotoFormatter'
+
 type CreateFileParams = {
   base64String: string
   fileName: string
   fileType: string
 }
 
-export const createFile = async ({
+export const createFile = ({
   base64String,
   fileName,
   fileType,
-}: CreateFileParams): Promise<File> => {
-  const response = await fetch(base64String)
-  const blob = await response.blob()
-  return new File([blob], fileName, { type: fileType })
+}: CreateFileParams): File => {
+  const uri = PhotoFormatter.formatUri({
+    filename: fileName,
+    extension: fileType,
+    file: base64String,
+  })
+
+  const arr = uri.split(',')
+  const mime = arr[0].match(/:(.*?);/)?.[1]
+
+  if (!mime) {
+    throw new Error('Invalid file type')
+  }
+
+  const bstr = atob(arr[arr.length - 1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+
+  return new File([u8arr], fileName, { type: mime })
 }
