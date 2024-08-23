9a3cc7627523b10de7ab3e4885559343dc2afd91
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-23 17:19:20 -0300

    feat: Add CreateAreaModal component for creating new areas

---
+++ src/components/modules/location/CreateArea.tsx
@@ -0,0 +1,87 @@
+import { Fragment } from 'react'
+
+import { useCreateArea } from '@/hooks/location/useCreateArea'
+
+import { Modal } from '@/components/layout/Modal'
+import { Button } from '@/components/elements/Button'
+import { Input } from '@/components/elements/Input'
+import { Checkbox } from '@/components/elements/Checkbox'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const CreateAreaModal: React.FC<Props> = ({ isOpen, onClose }) => {
+  const {
+    handleCreateArea,
+    isLoadingCreateArea,
+    register,
+    errors,
+    isActive,
+    handleActiveChange,
+  } = useCreateArea()
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Cadastrar área"
+      onSubmit={handleCreateArea(onClose)}
+      size="lg"
+      footer={
+        <Fragment>
+          <Button
+            title="Voltar"
+            itsCancelButton
+            additionalClasses="w-36 bg-gray-200 hover:bg-gray-300 active:bg-gray-400"
+            color="text-gray-700"
+            onClick={onClose}
+          />
+          <Button
+            title="Salvar"
+            additionalClasses="w-44 bg-cyan-800 hover:bg-cyan-900 active:bg-cyan-950"
+            isLoading={isLoadingCreateArea}
+            type="submit"
+          />
+        </Fragment>
+      }
+    >
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+        <div className="col-span-1 sm:col-span-2">
+          <Input
+            placeholder="Nome"
+            formError={errors.nome?.message}
+            name="nome"
+            autoFocus
+            register={register}
+          />
+        </div>
+        <Input
+          placeholder="Nome do responsável"
+          name="responsavelnome"
+          formError={errors.responsavelnome?.message}
+          register={register}
+          autoFocus
+        />
+        <Input
+          placeholder="Email do responsável"
+          type="email"
+          name="responsavelemail"
+          formError={errors.responsavelemail?.message}
+          register={register}
+          autoFocus
+        />
+        <div className="col-span-1 sm:col-span-2">
+          <Checkbox
+            label="Status"
+            description="Defina se a área está ativa ou não."
+            checked={isActive}
+            onChange={handleActiveChange}
+            formError={errors.ativo?.message}
+          />
+        </div>
+      </div>
+    </Modal>
+  )
+}
