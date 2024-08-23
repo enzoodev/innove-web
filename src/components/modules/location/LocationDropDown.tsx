bac590cc4cdc3a5f55fb9f23be17f0dff6e7f85d
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-23 17:20:17 -0300

    feat: Add handleOpen function to LocationDropDown component

--- src/components/modules/location/LocationDropDown.tsx
+++ src/components/modules/location/LocationDropDown.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { IconChevronDown } from '@tabler/icons-react'
 
 type Props = {
@@ -13,14 +14,24 @@ export const LocationDropDown: React.FC<Props> = ({
   onOpen,
   children,
 }) => {
+  const handleOpen = useCallback(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div>
-      <div className="flex flex-row items-center justify-between py-2 px-4 border-b border-gray-300">
+      <div
+        className="flex flex-row items-center justify-between py-2 px-4 border-b border-gray-300
+        hover:bg-white hover:bg-opacity-20 active:bg-white active:bg-opacity-40 cursor-pointer"
+        onClick={handleOpen}
+      >
         <h3 className="text-gray-800 text-md font-semibold">{title}</h3>
         {!isOpen && (
           <button
             type="button"
-            onClick={onOpen}
+            onClick={handleOpen}
             className="w-10 h-10 bg-gray-300 border border-gray-400 rounded-full flex items-center justify-center"
           >
             <IconChevronDown stroke={1.5} className="text-gray-700 w-7 h-7" />
