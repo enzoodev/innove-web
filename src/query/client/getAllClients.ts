aa3ffd8589b548b7ba60eaeec837819c9c0dbb7a
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-22 23:10:03 -0300

    refactor: Update getAllClients to use HttpServices for fetching clients

--- src/query/client/getAllClients.ts
+++ src/query/client/getAllClients.ts
@@ -5,5 +5,9 @@ export const getAllClients = async () => {
   // return HttpServices.get<Array<TClient>>({
   //   url: 'clients',
   // })
+  const clients = await HttpServices.get<Array<TClient>>({
+    url: 'clients',
+  })
+  console.log('clients', clients)
   return getClientsMock as Array<TClient>
 }
