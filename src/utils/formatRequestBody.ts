35716110ec96cb764b7f51ac6445768a9c5ad8b8
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-24 16:50:28 -0300

    refactor: Rename formatRequest to formatRequestBody for clarity and consistency

--- src/utils/formatRequestBody.ts
+++ src/utils/formatRequestBody.ts
@@ -1,4 +1,4 @@
-export const formatRequest = (data: any, formData = new FormData()) => {
+export const formatRequestBody = (data: any, formData = new FormData()) => {
   if (!data) {
     return null
   }
