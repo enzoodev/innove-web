153fb22885baeb749ebc162f68fe043a9506e805
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-24 16:51:19 -0300

    refactor: Update HttpServices to use formatRequestBody for request body formatting

--- src/query/client/getAllClients.ts
+++ src/query/client/getAllClients.ts
@@ -1,26 +1,7 @@
-// import { getClientsMock } from '@/mocks/get-clients-mock'
-// import { HttpServices } from '@/services/HttpServices'
-
-import { TokenService } from '@/services/TokenService'
-
-// export const getAllClients = async () => {
-//   // return HttpServices.get<Array<TClient>>({
-//   //   url: 'clients',
-//   // })
-//   await new Promise((resolve) => setTimeout(resolve, 1000))
-//   return getClientsMock as Array<TClient>
-// }
+import { HttpServices } from '@/services/HttpServices'
 
 export const getAllClients = async () => {
-  console.log('cuzinho')
-  console.log(TokenService.get())
-  const response = await fetch('/api/clients')
-  const data = await response.json()
-  console.log(data)
-  // if (!response.ok) {
-  //   throw new Error('Failed to fetch clients')
-  // }
-  // const data = await response.json()
-  // return data.data as Array<TClient>
-  return data
+  return HttpServices.get<Array<TClient>>({
+    url: 'clients',
+  })
 }
--- src/query/client/getClientById.ts
+++ src/query/client/getClientById.ts
@@ -1,13 +1,10 @@
-import { getClientMock } from '@/mocks/get-client-mock'
 import { HttpServices } from '@/services/HttpServices'
 
 export const getClientById = async (params: TGetClientParams) => {
-  // const [client] = await HttpServices.get<Array<TClient>>({
-  //   url: 'clients',
-  //   params,
-  // })
+  const [client] = await HttpServices.get<Array<TClient>>({
+    url: 'clients',
+    params,
+  })
 
-  // return client
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return getClientMock as TClient
+  return client
 }
