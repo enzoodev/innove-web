243e438d924298141cfdb3640a41ad89559085ff
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-25 14:36:57 -0300

    refactor: Replace PhotoFormatter with FileConverter for image URI formatting

--- src/utils/PhotoFormatter.ts
+++
@@ -1,8 +0,0 @@
-export class PhotoFormatter {
-  public static readonly formatUri = (photo?: TFile) => {
-    if (!photo) return ''
-
-    const fileType = photo.extension.toLowerCase()
-    return `data:${fileType};base64,${photo.file}`
-  }
-}
