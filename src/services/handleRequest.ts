e5ba64a857a711254756881e8d8eff8df02d05d1
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-25 14:44:03 -0300

    refactor: Update handleRequest to support file uploads in form data

--- src/services/handleRequest.ts
+++ src/services/handleRequest.ts
@@ -1,16 +1,44 @@
 import { NextApiRequest } from 'next'
+
 import { UrlBuilder } from '@/utils/UrlBuilder'
+import { getHeaders } from '@/utils/getHeaders'
+import { FileConverter } from '@/utils/FileConverter'
 
 export const handleRequest = async (req: NextApiRequest) => {
   const url = UrlBuilder.handleUrl(req.url ?? '')
+  const headers = getHeaders(req.method ?? '', req.headers.authorization ?? '')
+
+  const formData = new FormData()
+
+  if (req.body) {
+    const body = JSON.parse(req.body)
+    const { files, ...json } = body
+
+    formData.append('json', JSON.stringify(json))
+
+    if (files) {
+      Object.keys(files).forEach((key) => {
+        const fileUri = files[key]
+
+        const { base64String, fileType, fileName } =
+          FileConverter.extractFileDataFromUri(fileUri)
+
+        const file = FileConverter.base64ToFile({
+          base64String,
+          fileName,
+          fileType,
+        })
+
+        formData.append(key, file)
+      })
+    }
+  }
 
   const response = await fetch(url, {
     method: req.method,
+    body: req.body ? formData : null,
     credentials: 'include',
-    headers: {
-      Authorization: req.headers.authorization ?? 'no-token',
-      'Content-Type': req.headers['content-type'] ?? 'application/json',
-    },
+    headers,
   })
 
   const data = await response.json()
