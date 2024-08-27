7e4d02e213d9ab29a79de62538cc67150e5bedb5
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-27 14:17:05 -0300

    refactor: Update components for improved functionality and styling

--- src/pages/checklists/create/index.tsx
+++ src/pages/checklists/create/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next'
+import { Controller } from 'react-hook-form'
 import {
   IconArticle,
+  IconCheck,
   IconChevronDown,
   IconEdit,
   IconListNumbers,
@@ -17,18 +19,19 @@ import { LayoutApp } from '@/components/layout/LayoutApp'
 import { Input } from '@/components/elements/Input'
 import { Button } from '@/components/elements/Button'
 import { Select } from '@/components/elements/Select'
+import { Switch } from '@/components/elements/Switch'
 
 const CreateChecklist: NextPage = () => {
   const {
     handleCreateChecklist,
     isLoadingCreateChecklist,
-    register,
     errors,
+    control,
     sections,
-    type,
     isActive,
     handleActiveChange,
     handleToggleEditSectionTitle,
+    handleDoneEditingSectionTitle,
     handleAddQuestion,
     handleDeleteQuestion,
     handleOpenSection,
@@ -44,22 +47,44 @@ const CreateChecklist: NextPage = () => {
         <div key={section.id}>
           <div className="flex flex-col gap-4 py-3 px-4">
             <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row items-end gap-4 w-3/6">
+              <div className="flex flex-row items-center gap-4 w-3/6">
                 {section.isEditing ? (
-                  <Input
-                    key={section.id}
-                    placeholder={`Tópico ${sectionIndex + 1}`}
-                    formError={errors.sections?.[sectionIndex]?.title?.message}
-                    name={sections?.[sectionIndex]?.title}
-                    autoFocus
-                    register={register}
+                  <Controller
+                    control={control}
+                    name={`sections.${sectionIndex}.title`}
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        name={sections?.[sectionIndex]?.title}
+                        placeholder={`Tópico ${sectionIndex + 1}`}
+                        onChange={onChange}
+                        value={value}
+                        formError={
+                          errors.sections?.[sectionIndex]?.title?.message
+                        }
+                        button={
+                          <button
+                            onClick={() =>
+                              handleDoneEditingSectionTitle(sectionIndex)
+                            }
+                            type="button"
+                            className="flex items-center justify-center w-10 h-10 mb-1.5 rounded-full bg-gray-300 hover:bg-gray-400 active:bg-gray-400 hover:bg-opacity-30 active:bg-opacity-50"
+                          >
+                            <IconCheck
+                              stroke={1.5}
+                              className="w-6 h-6 text-gray-700"
+                            />
+                          </button>
+                        }
+                        autoFocus
+                      />
+                    )}
                   />
                 ) : (
                   <h3 className="text-base font-medium text-gray-700">
                     {section.title}
                   </h3>
                 )}
-                {section.isOpen && (
+                {section.isOpen && !section.isEditing && (
                   <button
                     onClick={() => handleToggleEditSectionTitle(sectionIndex)}
                     type="button"
@@ -72,7 +97,7 @@ const CreateChecklist: NextPage = () => {
               {section.isOpen ? (
                 <div className="flex flex-row gap-3">
                   <Button
-                    title="Adicionar pergunta"
+                    title="Adicionar questão"
                     itsCancelButton
                     additionalClasses="w-60 bg-zinc-700 hover:bg-zinc-800 active:bg-zinc-900"
                     color="text-white"
@@ -108,33 +133,41 @@ const CreateChecklist: NextPage = () => {
             {section.isOpen && (
               <div className="flex flex-col gap-4">
                 {section.items.map((question, questionIndex) => (
-                  <div key={question.id}>
-                    <Input
-                      key={question.id}
-                      placeholder={`Pergunta ${questionIndex + 1}`}
-                      formError={
-                        errors.sections?.[sectionIndex]?.items?.[questionIndex]
-                          ?.name?.message
-                      }
-                      name={
-                        sections?.[sectionIndex]?.items[questionIndex]?.name
-                      }
-                      autoFocus
-                      register={register}
-                    />
-                    <button
-                      type="button"
-                      onClick={() =>
-                        handleDeleteQuestion(sectionIndex, question.id)
-                      }
-                      className="w-10 h-10 bg-gray-300 border border-gray-400 rounded-full flex items-center justify-center"
-                    >
-                      <IconTrash
-                        stroke={1.5}
-                        className="text-gray-700 w-6 h-6"
+                  <Controller
+                    key={question.id}
+                    control={control}
+                    name={`sections.${sectionIndex}.items.${questionIndex}.name`}
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        name={
+                          sections?.[sectionIndex]?.items[questionIndex]?.name
+                        }
+                        placeholder={`Pergunta ${questionIndex + 1}`}
+                        onChange={onChange}
+                        value={value}
+                        formError={
+                          errors.sections?.[sectionIndex]?.items?.[
+                            questionIndex
+                          ]?.name?.message
+                        }
+                        button={
+                          <button
+                            type="button"
+                            onClick={() =>
+                              handleDeleteQuestion(sectionIndex, question.id)
+                            }
+                            className="w-10 h-10 bg-gray-300 border border-gray-400 rounded-full flex items-center justify-center"
+                          >
+                            <IconTrash
+                              stroke={1.5}
+                              className="text-gray-700 w-6 h-6"
+                            />
+                          </button>
+                        }
+                        autoFocus
                       />
-                    </button>
-                  </div>
+                    )}
+                  />
                 ))}
               </div>
             )}
@@ -150,7 +183,12 @@ const CreateChecklist: NextPage = () => {
       title="Cadastrar Checklist"
       headTitle="Checklists - Innove"
       hasCreateButton={false}
-      //   headerRightComponent={}
+      headerRightComponent={
+        <div className="flex flex-row items-center gap-2">
+          <h3 className="text-md font-medium text-gray-600">Status</h3>
+          <Switch isChecked={isActive} onChange={handleActiveChange} />
+        </div>
+      }
     >
       <div className="flex flex-col gap-4">
         <div className="bg-gray-100 rounded-md border border-gray-300 shadow-[0_3px_10px_rgb(0,0,0,0.1)]">
@@ -159,19 +197,31 @@ const CreateChecklist: NextPage = () => {
             <h2 className="text-lg font-medium text-gray-700">Informações</h2>
           </div>
           <div className="flex flex-row gap-4 p-4">
-            <Input
-              placeholder="Nome do checklist"
-              formError={errors.name?.message}
+            <Controller
+              control={control}
               name="name"
-              register={register}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChange={onChange}
+                  value={value}
+                  formError={errors.name?.message}
+                  autoFocus
+                />
+              )}
             />
-            <Select
-              placeholder="Tipo do checklist"
-              formError={errors.type?.message}
+            <Controller
+              control={control}
               name="type"
-              register={register}
-              value={type}
-              options={locationTypes}
+              render={({ field: { onChange, value } }) => (
+                <Select
+                  placeholder="Tipo"
+                  onChange={onChange}
+                  value={value}
+                  formError={errors.type?.message}
+                  options={locationTypes}
+                />
+              )}
             />
           </div>
         </div>
