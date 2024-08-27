b2237f16e710af68cf504f39ea2e2a13abe4be0e
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-27 15:37:36 -0300

    feat: Add GET_CHECKLIST_BY_ID query key to QueryKey enum

--- src/enums/QueryKey.ts
+++ src/enums/QueryKey.ts
@@ -3,5 +3,6 @@ export enum QueryKey {
   GET_CLIENTS = 'get-clients',
   GET_LOCATIONS = 'get-locations',
   GET_CHECKLISTS = 'get-checklists',
+  GET_CHECKLIST_BY_ID = 'get-checklist-by-id',
   GET_USERS = 'get-users',
 }
