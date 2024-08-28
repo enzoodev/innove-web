0d8989b4fa5f87a33eaa49891238f1afd9429bc1
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-08-28 09:48:39 -0300

    refactor: Add useRecoverAccount hook for handling account recovery functionality

--- src/pages/auth/recover-account/index.tsx
+++ src/pages/auth/recover-account/index.tsx
@@ -1,6 +1,10 @@
+import { useRecoverAccount } from '@/hooks/auth/useRecoverAccount'
 import { NextPage } from 'next'
 
 const RecoverAccount: NextPage = () => {
+  const { register, errors, handleRecoverAccount, isLoadingRecoverAccount } =
+    useRecoverAccount()
+
   return <div>Recover Account</div>
 }
 
